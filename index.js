08009cc880d664ac89a13b1c2e5dbeb11d774fc3
Taine <taine.meyer@hotmail.com>
2022-06-16 15:40:51 +1000

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const element = document.querySelector("#main");
+
+element.parentElement.removeChild(element);
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+
+newHeader.innerHTML = "Taine is the champion"
